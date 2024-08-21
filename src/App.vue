91dd85714685b52a5cefce44d765bf1918ca9ec0
karl <kardoskarlheinz11@gmail.com>
2024-08-22 00:39:22 +0200

    added more code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Task Management</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn to="/users" text>Users</v-btn>
+      <v-btn to="/tasks" text>Tasks</v-btn>
+    </v-app-bar>
+
     <v-main>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-main>
+
+    <v-footer app>
+      <v-col class="text-center">&copy; 2024 Task Management System</v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: 'App'
 }
 </script>
+
+<style>
+/* You can add global styles here */
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000'; // Replace with your Laravel server URL
+
 createApp(App)
   .use(vuetify)
   .use(router)
