b1780482f0559b55ba3b09cd2bd7ba1928d5a614
karl <kardoskarlheinz11@gmail.com>
2024-08-22 13:48:41 +0200

    introduced sweetalert

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,75 +1,100 @@
 <template>
     <v-container>
       <v-btn @click="$router.push('/tasks/create')" color="secondary">Create Task</v-btn>
-      <v-data-table :headers="headers.text" :items="tasks">
+      <v-data-table :headers="headers" :items="tasks">
         <template v-slot:item.actions="{ item }">
           <v-btn @click="$router.push(`/tasks/${item.id}/edit`)" color="blue">Edit</v-btn>
-          <v-btn @click="deleteTask(item.id)" color="red">Delete</v-btn>
+          <v-btn @click="confirmDeleteTask(item.id)" color="red">Delete</v-btn>
         </template>
       </v-data-table>
     </v-container>
   </template>
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      headers: [
-        { text: 'Task Name', value: 'task_name' },
-        { text: 'Description', value: 'description' },
-        { text: 'Schedule Date', value: 'schedule_date' },
-        { text: 'Priority', value: 'priority' },
-        { text: 'Assigned To', value: 'assigned_to' },
-        { text: 'Actions', value: 'actions', sortable: false }
-      ],
-      tasks: [],
-      users: {}
-    };
-  },
-  mounted() {
-    this.fetchUsers();
-    this.fetchTasks();
-  },
-  methods: {
-    async fetchUsers() {
-      try {
-        const response = await axios.get('/api/users');
-        this.users = response.data.reduce((acc, user) => {
-            console.log(user)
-          acc[user.id] = `${user.first_name} ${user.last_name}`;
-          return acc;
-        }, {});
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
+  
+  <script>
+  import axios from 'axios';
+  import Swal from 'sweetalert2';
+  
+  export default {
+    data() {
+      return {
+        headers: [
+          { text: 'Task Name', value: 'task_name' },
+          { text: 'Description', value: 'description' },
+          { text: 'Schedule Date', value: 'schedule_date' },
+          { text: 'Priority', value: 'priority' },
+          { text: 'Assigned To', value: 'assigned_to' },
+          { text: 'Actions', value: 'actions', sortable: false }
+        ],
+        tasks: [],
+        users: {}
+      };
     },
-    async fetchTasks() {
-      try {
-        const response = await axios.get('/api/tasks');
-        this.tasks = response.data.map(task => ({
-          id: task.id,
-          task_name: task.task_name,
-          description: task.description,
-          schedule_date: task.schedule_date,
-          priority: task.priority,
-          assigned_to: this.users[task.assigned_to] || 'Unknown', 
-          actions: 'actions'
-        }));
-        console.log('Tasks with assigned users:', this.tasks); 
-      } catch (error) {
-        console.error('Error fetching tasks:', error);
-      }
+    mounted() {
+      this.fetchUsers();
+      this.fetchTasks();
     },
-    async deleteTask(id) {
-      try {
-        await axios.delete(`/api/tasks/${id}`);
-        this.tasks = this.tasks.filter(task => task.id !== id);
-      } catch (error) {
-        console.error('Error deleting task:', error);
+    methods: {
+      async fetchUsers() {
+        try {
+          const response = await axios.get('/api/users');
+          this.users = response.data.reduce((acc, user) => {
+            acc[user.id] = `${user.first_name} ${user.last_name}`;
+            return acc;
+          }, {});
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      },
+      async fetchTasks() {
+        try {
+          const response = await axios.get('/api/tasks');
+          this.tasks = response.data.map(task => ({
+            id: task.id,
+            task_name: task.task_name,
+            description: task.description,
+            schedule_date: task.schedule_date,
+            priority: task.priority,
+            assigned_to: this.users[task.assigned_to] || 'Unknown',
+            actions: 'actions'
+          }));
+        } catch (error) {
+          console.error('Error fetching tasks:', error);
+        }
+      },
+      async confirmDeleteTask(id) {
+        const result = await Swal.fire({
+          title: 'Are you sure?',
+          text: "This action cannot be undone!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        });
+  
+        if (result.isConfirmed) {
+          this.deleteTask(id);
+        }
+      },
+      async deleteTask(id) {
+        try {
+          await axios.delete(`/api/tasks/${id}`);
+          this.tasks = this.tasks.filter(task => task.id !== id);
+          Swal.fire(
+            'Deleted!',
+            'The task has been deleted.',
+            'success'
+          );
+        } catch (error) {
+          console.error('Error deleting task:', error);
+          Swal.fire(
+            'Error!',
+            'There was an error deleting the task.',
+            'error'
+          );
+        }
       }
     }
   }
-}
-</script>
-
+  </script>
+  
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,58 +1,85 @@
 <template>
     <v-container>
       <v-btn @click="$router.push('/users/create')" color="primary">Create User</v-btn>
-      <v-data-table :headers="headers.text" :items="users">
+      <v-data-table :headers="headers" :items="users">
         <template v-slot:item.actions="{ item }">
           <v-btn @click="$router.push(`/users/${item.id}/edit`)" color="blue">Edit</v-btn>
-          <v-btn @click="deleteUser(item.id)" color="red">Delete</v-btn>
+          <v-btn @click="confirmDelete(item.id)" color="red">Delete</v-btn>
         </template>
       </v-data-table>
     </v-container>
   </template>
   
   <script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      headers: [
-        { text: 'First Name', value: 'first_name' },
-        { text: 'Last Name', value: 'last_name' },
-        { text: 'Email', value: 'email' },
-        { text: 'Phone Number', value: 'phone_number' },
-        { text: 'Actions', value: 'actions', sortable: false }
-      ],
-      users: []
-    };
-  },
-  mounted() {
-    this.fetchUsers();
-  },
-  methods: {
-    async fetchUsers() {
-      try {
-        const response = await axios.get('/api/users');
-        this.users = response.data.map(user => ({
-          id: user.id,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          email: user.email,
-          phone_number: user.phone_number,
-          actions: 'actions'
-        }));
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
+  import axios from 'axios';
+  import Swal from 'sweetalert2';
+  
+  export default {
+    data() {
+      return {
+        headers: [
+          { text: 'First Name', value: 'first_name' },
+          { text: 'Last Name', value: 'last_name' },
+          { text: 'Email', value: 'email' },
+          { text: 'Phone Number', value: 'phone_number' },
+          { text: 'Actions', value: 'actions', sortable: false }
+        ],
+        users: []
+      };
     },
-    async deleteUser(id) {
-      try {
-        await axios.delete(`/api/users/${id}`);
-        this.users = this.users.filter(user => user.id !== id);
-      } catch (error) {
-        console.error('Error deleting user:', error);
+    mounted() {
+      this.fetchUsers();
+    },
+    methods: {
+      async fetchUsers() {
+        try {
+          const response = await axios.get('/api/users');
+          this.users = response.data.map(user => ({
+            id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            phone_number: user.phone_number,
+            actions: 'actions'
+          }));
+        } catch (error) {
+          console.error('Error fetching users:', error);
+        }
+      },
+      async confirmDelete(id) {
+        const result = await Swal.fire({
+          title: 'Are you sure?',
+          text: "This action cannot be undone!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, delete it!'
+        });
+  
+        if (result.isConfirmed) {
+          this.deleteUser(id);
+        }
+      },
+      async deleteUser(id) {
+        try {
+          await axios.delete(`/api/users/${id}`);
+          this.users = this.users.filter(user => user.id !== id);
+          Swal.fire(
+            'Deleted!',
+            'The user has been deleted.',
+            'success'
+          );
+        } catch (error) {
+          console.error('Error deleting user:', error);
+          Swal.fire(
+            'Error!',
+            'There was an error deleting the user.',
+            'error'
+          );
+        }
       }
     }
   }
-}
-</script>
+  </script>
+  
